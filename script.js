bff95611de027a55a2a0e5adece93816483f45c9
Rowan <rowjw97@gmail.com>
2023-12-12 19:35:52 +0000

    Improved sliding logo labels.

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const sliders = document.querySelectorAll('.slider')
 
 const sliderHover = (event) => {
     //console.log("sliding")
-    console.log(event.target)
-    const target = event.target.parentElement.children.item(1); // The event target will almost always be the <img> element, so the sliding text is the element's sibling.
-    console.log(target)
-
+    let target;
+    if (event.target.classList.contains("lang-logo")) {
+        target = event.target.parentElement.children.item(1); // The event target will almost always be the <img> element, so the sliding text is the element's sibling.
+    } else if (event.target.classList.contains("social-logo")) {
+        target = event.target.parentElement.parentElement.children.item(1); // The social logos are nested within <a> tags, so we need to go one lever higher up the DOM to find the correct child element.        
+    }
+    
     if (target.classList.contains("slider-text")) {
         target.style.opacity = 100;
         target.style.bottom = 0;
@@ -17,8 +20,10 @@ const sliderHoverOff = (event) => {
    const target = event.target.children.item(1);
 
    if (target.classList.contains("slider-text")) {
-        target.style.bottom = "30px";
-        target.style.opacity = 0;
+        setTimeout(function() {
+            target.style.bottom = "30px";
+            target.style.opacity = 0;
+        }, 1000)
    }
 }
 
