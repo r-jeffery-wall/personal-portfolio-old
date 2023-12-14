f4155b945629dac20fee9dfb2c522a3ac42ea611
Rowan <rowjw97@gmail.com>
2023-12-14 20:07:12 +0000

    Implemented text highlighting.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const sliders = document.querySelectorAll('.slider')
+const sliders = document.querySelectorAll('.slider');
+const langLogos = document.querySelectorAll('.lang-logo');
+const searchElements = document.querySelectorAll('p');
 
 const sliderHover = (event) => {
     //console.log("sliding")
@@ -27,7 +29,36 @@ const sliderHoverOff = (event) => {
    }
 }
 
+// Below could probably be better achieved using a framework. Come back to this.
+const highlight = (event) => { //https://stackoverflow.com/questions/52743841/find-and-highlight-word-in-text-using-js
+    const targetText = event.target.id;
+    let regex = new RegExp(targetText, 'gi');
+
+    if (event.target.classList.contains("highlighted")) { //Turns the text highlight off if it is already enabled.
+        event.target.classList.remove("highlighted");
+        highlightOff(targetText);
+    } else {
+        searchElements.forEach(element => {
+            event.target.classList.add("highlighted"); // This is a non-styled class that is just used as a marker.
+            let innerText = element.innerHTML;
+            element.innerHTML = innerText.replace(regex, `<mark>$&</mark>`);
+        });
+    };
+}
+
+const highlightOff = (targetText) => { // Helper function to turn off text-highlighting.
+    let regex = new RegExp(`<mark>${targetText}</mark>`, 'gi');
+    searchElements.forEach(element => {
+        let innerText = element.innerHTML;
+        element.innerHTML = innerText.replace(regex, targetText);
+    })
+}
+
 sliders.forEach(element => {
     element.addEventListener('mouseover', sliderHover);
     element.addEventListener('mouseleave', sliderHoverOff);
 });
+
+langLogos.forEach(element => {
+    element.addEventListener('click', highlight)
+})
