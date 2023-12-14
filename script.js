ffd6d8e382fd51b90a7fc11299231a0886cc0d9e
Rowan <rowjw97@gmail.com>
2023-12-14 20:23:23 +0000

    Refactored sliding labels functions.

--- script.js
+++ script.js
@@ -19,7 +19,12 @@ const sliderHover = (event) => {
 
 const sliderHoverOff = (event) => {
    //console.log("sliding off.")
-   const target = event.target.children.item(1);
+   let target;
+    if (event.target.classList.contains("lang-logo")) {
+        target = event.target.parentElement.children.item(1); // The event target will almost always be the <img> element, so the sliding text is the element's sibling.
+    } else if (event.target.classList.contains("social-logo")) {
+        target = event.target.parentElement.parentElement.children.item(1); // The social logos are nested within <a> tags, so we need to go one lever higher up the DOM to find the correct child element.        
+    }
 
    if (target.classList.contains("slider-text")) {
         setTimeout(function() {
